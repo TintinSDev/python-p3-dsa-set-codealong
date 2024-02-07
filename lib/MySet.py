b3d23413f620b0497abe576a8a6cbaaad81f802a
Martin <mmaina290@gmail.com>
2024-02-07 17:53:00 +0300

    set

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,20 @@
 class MySet:
 
-    pass
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary.put(value) = True
+      
+    def has(self, value):
+        return value in self.dictionary
+  
+    def add(self, value):
+        self.dictionary[value] = True # Add a value as a key on the Dictionary
+        return self  
+
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size:
+    len(self.dictionary)
